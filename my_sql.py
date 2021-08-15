a643bc753063e00167f275577aa9bb9615b5d8fa
jamesmcc90 <james.mcc90@gmail.com>
2021-08-15 11:29:10 +0100

    Updated to prompt for username/password for database

--- my_sql.py
+++ my_sql.py
@@ -2,33 +2,41 @@
 
 # Create a Function to define server connection details
 
+from logging import error
 import mysql.connector
-#customerName = input("Enter Customer Name: ")
-#customerAddress = input("Enter Customer Address: ")
+from mysql.connector import Error, connection
+from getpass import getpass
 
-carID = input("Enter Car ID: ")
-carColour = input("Enter Car Colour: ")
-carMake = input("Enter Car Make: ")
-carModel = input("Enter Car Model: ")
+try:
+    with mysql.connector.connect(
+        host='localhost',
+        user=input('Enter username: '), # root,
+        password=getpass('Enter password: '), #64lislunnan,
+        database='my_schema',
+        auth_plugin='mysql_native_password'
+    ) as connection:        
+        mycursor = connection.cursor()
+        print(connection)
+except Error as cError:
+    print(cError)
 
-connection = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='64lislunnan',
-    database='my_schema',
-    auth_plugin='mysql_native_password'
-)
-
-mycursor = connection.cursor()
+if (connection):
+    carID = input("Enter Car ID: ")
+    carColour = input("Enter Car Colour: ")
+    carMake = input("Enter Car Make: ")
+    carModel = input("Enter Car Model: ")
+else:
+    print("Sorry, access denied!")
 
 # Create a function defining SQL insert
 def sql():
     query = "INSERT INTO cars (Car_ID, Car_Colour, Car_Make, Car_Model) VALUES (%s, %s, %s, %s)"
     values = (carID, carColour, carMake, carModel)
+    connection.connect()
     mycursor.execute(query, values)
+    connection.commit()  # Make changes persistent to DB/Table
 
 sql()
-connection.commit()  # Make changes persistent to DB/Table
 
 for x in mycursor:
     print(x)
