5c6c1c9dd53bd0e846911592d7473496bf0b66f7
jamesmcc90 <james.mcc90@gmail.com>
2021-08-16 21:09:43 +0100

    Brushed up on tkinter

--- gui_app.py
+++ gui_app.py
@@ -8,32 +8,38 @@ from tkinter import *
 import tkinter
 import tkinter.messagebox
 
-class Application(tkinter.Frame):
+baseWindow = tkinter.Tk()
+baseWindow.title("My Python GUI Application")
+baseWindow.iconbitmap('C:\\Users\\James\\Downloads\\Cornmanthe3rd-Plex-Other-python.ico')
+baseWindow.geometry('400x300')
 
-    baseWindow = tkinter.Tk()
+Label(baseWindow, text="Car ID", font=(12)).grid(row=0, column=0)
+inputID = tkinter.Entry(baseWindow)
+inputID.grid(row=0, column=1)
 
-    Label(baseWindow, text="Car ID", font=(12)).grid(row=0, column=0)
-    inputID = tkinter.Entry(baseWindow)
-    inputID.grid(row=0, column=1)
+'''
+def number():
+    try:
+        int(inputID.get())
+    except ValueError:
+        errorLabel.config(baseWindow, text='That is text!', font=(12)).grid(row=10, column=0)
+'''
 
-    Label(baseWindow, text="Car Colour", font=(12)).grid(row=1, column=0)
-    inputColour = tkinter.Entry(baseWindow)
-    inputColour.grid(row=1, column=1)
+errorLabel = Label(baseWindow, text="")
+Label(baseWindow, text="Car Colour", font=(12)).grid(row=2, column=0)
+inputColour = tkinter.Entry(baseWindow)
+inputColour.grid(row=2, column=1)
 
-    Label(baseWindow, text="Car Make", font=(12)).grid(row=2, column=0)
-    inputMake = tkinter.Entry(baseWindow)
-    inputMake.grid(row=2, column=1)
+Label(baseWindow, text="Car Make", font=(12)).grid(row=3, column=0)
+inputMake = tkinter.Entry(baseWindow)
+inputMake.grid(row=3, column=1)
 
-    Label(baseWindow, text="Car Model", font=(12)).grid(row=3, column=0)
-    inputModel = tkinter.Entry(baseWindow)
-    inputModel.grid(row=3, column=1)
-        
-    baseWindow.title("My Python GUI Application")
+Label(baseWindow, text="Car Model", font=(12)).grid(row=4, column=0)
+inputModel = tkinter.Entry(baseWindow)
+inputModel.grid(row=4, column=1)
 
-    def hello ():
-     tkinter.messagebox.showinfo("Message Box", "Hello there!")
+def hello ():
+    tkinter.messagebox.showinfo("Message Box", "Hello there!")
 
-    myButton = tkinter.Button(text="Click Me", command=hello).grid(row=5, column=0)
-
-    baseWindow.geometry('600x600')
-    baseWindow.mainloop()
+myButton = tkinter.Button(text="Click Me", command=quit).grid(row=6, column=0)
+baseWindow.mainloop()
